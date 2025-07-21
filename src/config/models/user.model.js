a450b21deba2300e467340e0f8651211182a288f
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2025-07-21 16:57:32 +0300

    feat(models): add User model

---
+++ src/config/models/user.model.js
@@ -0,0 +1,58 @@
+import { Schema, model } from "mongoose";
+
+export const Roles = {
+  admin: "admin",
+  user: "user",
+};
+Object.freeze(Roles);
+
+export const Gender = {
+  male: "male",
+  female: "female",
+};
+Object.freeze(Gender);
+
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please provide a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      maxlength: 20,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: Object.values(Roles),
+      default: Roles.user,
+    },
+    gender: {
+      type: String,
+      default: Gender.male,
+      enum: Object.values(Gender),
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = model("users", schema);
+export default userModel;
